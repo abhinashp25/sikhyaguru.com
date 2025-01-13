0db47dd32eb397e80714fcc7dca961e6c576b9bf
suryadiamond341 <suryamanipatra114@gmail.com>
2025-01-13 15:43:17 +0530

    Created a new branch surya and pushed the frontend setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="bg-blue-500 text-white text-center p-8 rounded">
-    <h1 className="text-4xl font-bold">Hello, Welocome to SikshyaGuru!</h1>
-    <p className="mt-4">Find you drim Colllege here...</p>
-  </div>
+      <h1 className="text-4xl font-bold">Hello, Welocome to SikshyaGuru!</h1>
+      <p className="mt-4">Find you drim Colllege here...</p>
+    </div>
   );
 }
 
