234a0a0a17f71650ac5508ce5223c036715b3a7b
suryadiamond341 <suryamanipatra114@gmail.com>
2025-01-13 11:13:55 +0530

    Frontend Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white text-center p-8 rounded">
+    <h1 className="text-4xl font-bold">Hello, Welocome to SikshyaGuru!</h1>
+    <p className="mt-4">Find you drim Colllege here...</p>
+  </div>
+  );
+}
+
+export default App;
