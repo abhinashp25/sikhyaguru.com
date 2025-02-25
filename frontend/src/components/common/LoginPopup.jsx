1720a2fd45096d07e33349ce1ca494b89d1ff14e
Lalit <kumarlalitamohanty@gmail.com>
2025-02-25 20:19:23 +0530

    ui changes 13

--- frontend/src/components/common/LoginPopup.jsx
+++ frontend/src/components/common/LoginPopup.jsx
@@ -362,7 +362,7 @@ const LoginPopup = () => {
         <div className="fixed inset-0   flex items-center justify-center bg-black/50 backdrop-blur-sm z-50">
           <div
           //  style={{ backgroundImage: `url(${EntrollForm}) ` }}
-           className="relative   bg-gray-50  h-[90vh] m-1 md:m-0 overflow-y-auto  md:h-fit p-2 md:p-2 rounded-lg shadow-lg w-full max-w-4xl flex flex-col md:flex-row">
+           className="relative   bg-gray-50  h-fit m-1 md:m-0 overflow-y-auto  md:h-fit p-2 md:p-2 rounded-lg shadow-lg w-full max-w-4xl flex flex-col md:flex-row">
             {/* Left Section: Image and Info */}
             <div
               style={{ backgroundImage: `url(${EntrollForm}) ` }}
