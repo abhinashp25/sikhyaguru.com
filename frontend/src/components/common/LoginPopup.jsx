2da04bedcaf44a48cde021edfc44499cea9d25b6
Lalit <kumarlalitamohanty@gmail.com>
2025-03-12 10:24:28 +0530

    changes in loginpupup

--- frontend/src/components/common/LoginPopup.jsx
+++ frontend/src/components/common/LoginPopup.jsx
@@ -336,7 +336,7 @@ const LoginPopup = () => {
     if (!isAuth) {
       const interval = setInterval(() => {
         setIsOpen(true);
-      }, 5000);
+      }, 15000);
       return () => clearInterval(interval);
     }
   }, [isAuth]);
@@ -382,127 +382,187 @@ const LoginPopup = () => {
               /> */}
             </div>
             {/* Right Section: Form */}
-          
-            <div className="w-full md:w-1/2 p-6">
+
+           
+
+            <div className="w-full md:w-1/2 p-3 md:p-6">
               <div className="flex justify-between items-center mb-4">
-                <h2 className="text-xl text-center font-semibold">
+                <h2 className="text-xl mx-auto container text-center font-semibold">
                   Enrollment Form
                 </h2>
-                <X
+                <button
                   onClick={onClose}
                   className="cursor-pointer text-gray-500 hover:text-red-500"
-                  size={24}
-                />
+                >
+                  ✖
+                </button>
               </div>
+
               <form onSubmit={handleSubmit(onSubmit)} className="grid gap-4">
                 {/* Full Name */}
-                <div>
-                <input
-                  type="text"
-                  placeholder="Full Name"
-                  {...register("name", { required: "Name is required" })}
-                  className={`border rounded-md p-3 w-full ${
-                    errors.name ? "border-red-500" : ""
-                  }`}
-                />
-                {errors.name && (
+                <div className="relative">
+                  <input
+                    type="text"
+                    {...register("name", { required: "Name is required" })}
+                    className={`peer border rounded-md p-2 md:p-3 w-full bg-transparent focus:outline-none focus:ring-1 ${
+                      errors.name
+                        ? "border-red-500 focus:ring-red-500"
+                        : "border-gray-300 focus:ring-blue-500"
+                    }`}
+                    placeholder=" "
+                  />
+                  <label
+                    className={`absolute left-3 px-1 bg-white transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-2 peer-focus:text-xs peer-focus:text-blue-600 -top-2 text-xs ${
+                      errors.name
+                        ? "text-red-500 peer-focus:text-red-500"
+                        : "text-gray-500"
+                    }`}
+                  >
+                    Full Name
+                  </label>
+                  {errors.name && (
                   <span className="text-red-500 text-xs">
                     {errors.name.message}
                   </span>
                 )}
                 </div>
+               
 
                 {/* Email */}
-                <div>
-                <input
-                  type="email"
-                  placeholder="Email"
-                  {...register("email", {
-                    required: "Email is required",
-                    pattern: {
-                      value:
-                        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-                      message: "Invalid email address",
-                    },
-                  })}
-                  className={`border rounded-md p-3 w-full ${
-                    errors.email ? "border-red-500" : ""
-                  }`}
-                />
-                {errors.email && (
+                <div className="relative">
+                  <input
+                    type="email"
+                    {...register("email", { required: "Email is required" })}
+                    className={`peer border rounded-md  p-2 md:p-3  w-full bg-transparent focus:outline-none focus:ring-1 ${
+                      errors.email
+                        ? "border-red-500 focus:ring-red-500"
+                        : "border-gray-300 focus:ring-blue-500"
+                    }`}
+                    placeholder=" "
+                  />
+                  <label
+                    className={`absolute left-3 px-1 bg-white transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-2 peer-focus:text-xs peer-focus:text-blue-600 -top-2 text-xs ${
+                      errors.email
+                        ? "text-red-500 peer-focus:text-red-500"
+                        : "text-gray-500"
+                    }`}
+                  >
+                    Email
+                  </label>
+                  {errors.email && (
                   <span className="text-red-500 text-xs">
                     {errors.email.message}
                   </span>
                 )}
-                </div>
 
+                </div>
+              
                 {/* Phone Number */}
-                <div>
-                <input
-                  type="tel"
-                  placeholder="Phone Number"
-                  {...register("phone", {
-                    required: "Phone number is required",
-                    pattern: {
-                      value: /^[6-9]\d{9}$/,
-                      message: "Enter a valid 10-digit Indian number",
-                    },
-                  })}
-                  className={`border rounded-md p-3 w-full ${
-                    errors.phone ? "border-red-500" : ""
-                  }`}
-                />
-                {errors.phone && (
+                <div className="relative">
+                  <input
+                    type="tel"
+                    {...register("phone", {
+                      required: "Phone number is required",
+                      pattern: {
+                        value: /^[6-9]\d{9}$/,
+                        message: "Enter a valid Indian phone number",
+                      },
+                      maxLength: 10,
+                    })}
+                    className={`peer border rounded-md  p-2 md:p-3  w-full bg-transparent focus:outline-none focus:ring-1 ${
+                      errors.phone
+                        ? "border-red-500 focus:ring-red-500"
+                        : "border-gray-300 focus:ring-blue-500"
+                    }`}
+                    placeholder=" "
+                    maxLength={10}
+                  />
+                  <label
+                    className={`absolute left-3 px-1 bg-white transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-2 peer-focus:text-xs peer-focus:text-blue-600 -top-2 text-xs ${
+                      errors.phone
+                        ? "text-red-500 peer-focus:text-red-500"
+                        : "text-gray-500"
+                    }`}
+                  >
+                    Phone Number
+                  </label>
+                  {errors.phone && (
                   <span className="text-red-500 text-xs">
                     {errors.phone.message}
                   </span>
                 )}
                 </div>
+               
 
                 {/* City */}
-                <div>
-                <input
-                  type="text"
-                  placeholder="City"
-                  {...register("city", { required: "City is required" })}
-                  className={`border rounded-md p-3 w-full ${
-                    errors.city ? "border-red-500" : ""
-                  }`}
-                />
-                {errors.city && (
+                <div className="relative">
+                  <input
+                    type="text"
+                    {...register("city", { required: "City is required" })}
+                    className={`peer border rounded-md  p-2 md:p-3  w-full bg-transparent focus:outline-none focus:ring-1 ${
+                      errors.city
+                        ? "border-red-500 focus:ring-red-500"
+                        : "border-gray-300 focus:ring-blue-500"
+                    }`}
+                    placeholder=" "
+                  />
+                  <label
+                    className={`absolute left-3 px-1 bg-white transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-2 peer-focus:text-xs peer-focus:text-blue-600 -top-2 text-xs ${
+                      errors.city
+                        ? "text-red-500 peer-focus:text-red-500"
+                        : "text-gray-500"
+                    }`}
+                  >
+                    City
+                  </label>
+                  {errors.city && (
                   <span className="text-red-500 text-xs">
                     {errors.city.message}
                   </span>
                 )}
                 </div>
+             
 
                 {/* Course Selection */}
-                <div>
-                <select
-                  {...register("course", {
-                    required: "Course selection is required",
-                  })}
-                  className={`border rounded-md p-3 w-full ${
-                    errors.course ? "border-red-500" : ""
-                  }`}
-                >
-                  <option value="">Select Course</option>
-                  <option value="BTech">BTech</option>
-                  <option value="MTech">MTech</option>
-                  <option value="MCA">MCA</option>
-                  <option value="BCA">BCA</option>
-                </select>
-                {errors.course && (
+                <div className="relative">
+                  <select
+                    {...register("course", {
+                      required: "Course selection is required",
+                    })}
+                    className={`peer border rounded-md  p-2 md:p-3  w-full bg-transparent focus:outline-none focus:ring-1 ${
+                      errors.course
+                        ? "border-red-500 focus:ring-red-500"
+                        : "border-gray-300 focus:ring-blue-500"
+                    }`}
+                  >
+                    <option value="">Select Course</option>
+                    <option value="BTech">BTech</option>
+                    <option value="MTech">MTech</option>
+                    <option value="MCA">MCA</option>
+                    <option value="BCA">BCA</option>
+                    <option value="Other">More...</option>
+                  </select>
+                  <label
+                    className={`absolute left-3 px-1 bg-white transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-2 peer-focus:text-xs peer-focus:text-blue-600 -top-2 text-xs ${
+                      errors.course
+                        ? "text-red-500 peer-focus:text-red-500"
+                        : "text-gray-500"
+                    }`}
+                  >
+                    Course Interested
+                  </label>
+                  {errors.course && (
                   <span className="text-red-500 text-xs">
                     {errors.course.message}
                   </span>
                 )}
                 </div>
+              
 
                 {/* Submit Button */}
                 <button
                   type="submit"
-                  className="bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700"
+                  className="bg-gradient-to-br from-blue-700  via-blue-950 to-blue-700 text-white py-2 rounded-md hover:bg-blue-700"
                   disabled={loading}
                 >
                   {loading ? "Submitting..." : "Enroll Now"}
